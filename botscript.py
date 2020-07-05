9e2b98f2d1e06506ed33b420c3414beb78313b19
BK-Modding <kavesbteja@gmail.com>
2020-07-05 21:13:29 +0530

    Moved environment to conda, updated packages and dependencies, rewriting botscript.py

--- botscript.py
+++ botscript.py
@@ -1,71 +1,97 @@
-import discord
 import subprocess
 import re
 import os
 import asyncio
 import argparse
+
+from discord import Client
+from discord.ext import commands
 from googletrans import Translator
+
 import config
 
 TOKEN = config.TOKEN
 
-client = discord.Client()
-        
-	
+client = commands.Bot(command_prefix='!')
+
 @client.event
 async def on_error(event, *args, **kwargs):
     print("Error?")
+    
+async translate()
 
-@client.event
-async def on_message(message):
+async def background_translate(text, langs, times):
+    translator = Translator()
+    translation = text
+    count = 0
+    for i in range(times):
+        for lang in langs:
+            count += 1
+            translation = translator.translate(translation, dest=lang).text
+        translation = translator.translate(translation, dest='en').text
+
+    print("Count: {}".format(count))
+    
+    endtext = '[*] "{}" but translated {} times!'.format(text.strip(), times*len(langs)) + "\n\n" + translation
+    print("Output ready: {}".format(endtext))
+    
+    return endtext
+
+@client.command()
+async def translate(ctx):
+    message = ctx.message
+    
     # do not want the bot to reply to itself so
     if message.author == client.user:
         return
-    if message.content.startswith('!translate'):
-        message.content = '{}'.format(message.content)
-        args = message.content.split('-')
-        args = args[1:]
-        langs = []
-        n = -1
-        text = ""
-        for elem in args:
-            elem = '-' + elem.strip()
-            if elem.find('-l') != -1:
-                templangs = elem.split(' ')[1:]
-                print(templangs)
-            if elem.find('-n') != -1:
-                n = int(elem.split(' ')[1])
-                print(n)
-            if elem.find('-t') != -1:
-                try:
-                    text = re.findall(r'"([^"]*)"', elem)[0] # regex for life
-                except:
-                    text = "error in the text"
-                print(text)
-                
-        if n == -1:
-            n = 10
-        if len(langs) == 0:
-            langs = default=['de', 'ko', 'la', 'ja', 'eo']
-        if len(text) == 0:
-            text = "no text"
-       
-        client.loop.create_task(background_translate(message.channel, text, langs, n)) 
-        
-async def background_translate(channel, text, langs, times):
-    await client.wait_until_ready()
-    #channel = discord.Object(id=channel)
-    translator = Translator()
-    translation = text
-    for i in range(times):
-        for lang in langs:
-            translation = translator.translate(translation, dest=lang).text
-        translation = translator.translate(translation, dest='en').text
-		
-    endtext = '[*] "{}" but translated {} times!'.format(text.strip(), times*len(langs)) + "\n\n" + translation
-    print("output sent")
-    await client.send_message(channel, endtext)
-        
+    
+    message.content = '{}'.format(message.content)
+    
+    args = message.content.split('-')[1:]
+    args = [arg.strip() for arg in args]
+    
+    langs = []
+    n = -1
+    text = ""
+    
+    print("=" * 50)
+    print("New message received")
+    
+    for arg in args:
+        if arg.startswith('l'):
+            langs = arg.split(' ')[1:]
+            if len(langs) == 0:
+                return await ctx.send("Invalid language codes (-l): {}".format(message.content))
+            
+            print("Languages: {}".format(langs))
+        if arg.startswith('n'):
+            try:
+                n = int(arg.split(' ')[1])
+            except ValueError:
+                return await ctx.send("Invalid no. of translations (-n): {}".format(message.content))
+            
+            print("No. of translations: {}".format(n))
+        if arg.startswith('t'):
+            try:
+                text = re.findall(r'"([^"]*)"', arg)[0] # regex for life
+            except:
+                return await ctx.send("Error in the text (-t): {}".format(message.content))
+            
+            print("Translation text: {}".format(text))
+            
+    if n == -1:
+        n = 10   
+    if len(langs) == 0:
+        langs = ['de', 'ko', 'la', 'ja', 'eo'] # default
+    if len(text) == 0:
+        return await ctx.send("No text provided (-t): {}".format(message.content))
+    
+    print(text, langs, n)
+    
+    translated = await background_translate(text, langs, n)
+    
+    await ctx.send(translated)
+    
 @client.event
 async def on_ready():
     print('Logged in as')
